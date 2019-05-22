83b92a6f01c54e063356fbaf095e3fa83b69c455
Maxim Parygin <mparygin@yandex.ru>
2019-05-22 19:10:58 +0600

    I inverted the meaning of signal BLANK and rename it to DE (data enable)

--- vga_buffer.v
+++ vga_buffer.v
@@ -7,12 +7,12 @@ module vga_buffer #(parameter WIDTH = 0)(
   input wire in_vsync,
   input wire [WIDTH-1:0] in_hdata,
   input wire [WIDTH-1:0] in_vdata,
-  input wire in_blank,
+  input wire in_de,
   output reg out_hsync,
   output reg out_vsync,
   output reg [WIDTH-1:0] out_hdata,
   output reg [WIDTH-1:0] out_vdata,
-  output reg out_blank
+  output reg out_de
 );
 
 always @ (posedge clk)
@@ -21,7 +21,7 @@ begin
   out_vsync <= in_vsync;
   out_hdata <= in_hdata;
   out_vdata <= in_vdata;
-  out_blank <= in_blank;
+  out_de <= in_de;
 end
 
 endmodule
